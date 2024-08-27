12d8c399d145d09173c7e34dffec6f4b51b4d1fd
Piotr Sitarz <piotr.sitarz@rb.com>
2024-08-27 12:11:37 +0200

    chore: handle suspense for fetching data

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,11 @@
-export default function Page() {
+import {
+  fetchRevenue,
+  fetchLatestInvoices,
+  fetchCardData,
+} from "@/app/lib/data";
+
+export default async function Page() {
+  const latestInvoices = await fetchLatestInvoices();
+
   return <p>Invoices Page</p>;
 }
